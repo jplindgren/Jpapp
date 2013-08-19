6fe0f7bac902a4725c03b2251f7154cf92b1c8a3
Joao Paulo Lindgren <jplindgrenbr@hotmail.com>
2013-08-18 21:27:13 -0300

    Commit first source code

---
+++ src/com/jplindgren/jpapp/httpconnection/HttpNetworkConnection.java
@@ -0,0 +1,28 @@
+package com.jplindgren.jpapp.httpconnection;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class HttpNetworkConnection {
+	private ConnectivityManager connectivityService;
+	public HttpNetworkConnection(ConnectivityManager connectivityService){
+		this.connectivityService = connectivityService;
+	}
+	
+	public boolean CheckConnection(){
+		NetworkInfo networkInfo = this.connectivityService.getActiveNetworkInfo();
+		
+	    if (networkInfo != null && networkInfo.isConnected()) {
+	        return true;
+	    }
+	    return false;
+	}
+	
+	public boolean CheckWifiConnection(){
+		NetworkInfo networkInfo = connectivityService.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (networkInfo != null && networkInfo.isConnected()) {
+	        return true;
+	    }
+	    return false;
+	}
+} //class
